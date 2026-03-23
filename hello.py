9b575bebbd19b2200efa8ed9bd2e7de11d70a040
CFischer-Tech <fischer.j.corey@gmail.com>
2026-03-22 21:40:47 -0500

    Add error handling to hello.py (fixes #1)

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+def main():
+    try:
+        print("Hello, World!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
